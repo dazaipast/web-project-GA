6e33fa1c1b0336dd8b637577707db710e3ec97b3
dazaipast <dazaipast@gmail.com>
2026-09-17 14:45:46 +0300

    update button

--- js/script.js
+++ js/script.js
@@ -14,4 +14,14 @@ if (leadForm) {
       'Учебная форма проверена. Данные не отправлены.';
   });
 }
+const programCta = document.querySelector('#program-cta');
+if (programCta) {
+  programCta.addEventListener('click', () => {
+    document.querySelector('#program-preview').hidden = false;
+    gtag('event', 'cta_click', {
+      button_name: 'program',
+      page_section: 'hero'
+    });
+  });
+}
 
